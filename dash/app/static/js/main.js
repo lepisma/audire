8500cb936ef81e295e5e8299a6a7f47185399913
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2015-02-02 22:43:57 +0530

    Add settings modal

--- dash/app/static/js/main.js
+++ dash/app/static/js/main.js
@@ -1,1 +1,16 @@
 // Custom js stuff
+
+$(document).ready(function(){
+    // Hide overlay
+    $("#overlay").hide();
+
+    // Modal popup
+    $("#settings-btn").click(function(){
+        $("#overlay").fadeIn();
+    })
+
+    // Modal fadeout
+    $("#overlay-close-btn").click(function(){
+        $("#overlay").fadeOut();
+    });
+});
